40c1bf5972c6db46d4ffb0295d21b1e323e8c7e9
hend <87528987+hendsaeedd@users.noreply.github.com>
2023-04-27 13:04:25 +0200

    upload files

---
+++ project/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using com.calitha.goldparser;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        MyParser M;
+        public Form1()
+        {
+            InitializeComponent();
+            M = new MyParser("pld1.cgt",listBox1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            M.Parse(textBox1.Text);
+        }
+    }
+}
